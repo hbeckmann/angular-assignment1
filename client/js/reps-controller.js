9d867db4cbb55433d65adee8e515fd803a458929
hbeckmann <hunterbeckmann@gmail.com>
2015-09-22 09:31:22 -0600

    made search dynamic with bind

--- client/js/reps-controller.js
+++ client/js/reps-controller.js
@@ -3,44 +3,64 @@
 
   var self = this;
   self.members = [];
+  self.congressType = 'reps';
 
 
     self.searchByZip = function(zip) {
-      reps.repHttp(zip, '/all/by-zip/').then(function (data){
+      reps.allByZip(zip).then(function (data){
         self.members = data;
       });
     };
 
     self.searchRepByState = function(state) {
-      reps.repHttp(state, '/reps/by-state/').then(function (data){
+      reps.repsByState(state).then(function (data){
           self.members = data;
       });
     };
 
     self.searchRepByName = function(name) {
-      reps.repHttp(name, '/reps/by-name/').then(function (data){
+      reps.repsByName(name).then(function (data){
           self.members = data;
         });
     };
 
 
   self.searchSenByState = function(state) {
-    reps.repHttp(state, '/sens/by-state/').then(function (data){
+    reps.sensByState(state).then(function (data){
         self.members = data;
       });
   };
 
 
 self.searchSenByName = function(name) {
-  reps.repHttp(name, '/sens/by-name/').then(function (data){
+  reps.sensByName(name).then(function (data){
       self.members = data;
     });
 };
+
 });
 
 
   app.factory('reps', function ($http){
     var host = 'http://dgm-representatives.herokuapp.com';
+
+    function search(type, criteria, query){
+      return $http
+        .get(host + '/' + type + '/by-' + criteria + "/" + query)
+        .then(function (response){
+          return response.data;
+        })
+    };
+
+    search.allByZip = search.bind(null, 'all', 'zip');
+    search.repsByName = search.bind(null, 'reps', 'name');
+    search.repsByState = search.bind(null, 'reps', 'state');
+    search.sensBystate = search.bind(null, 'sens', 'state');
+    search.sensByName = search.bind(null, 'sens', 'name');
+
+    return search;
+
+    /*
     return {
       repHttp: function (type, path){
         return $http
@@ -48,6 +68,7 @@ self.searchSenByName = function(name) {
         .then(function (res){
           return res.data;
         });
-      }
     };
+  };
+    */
   });
